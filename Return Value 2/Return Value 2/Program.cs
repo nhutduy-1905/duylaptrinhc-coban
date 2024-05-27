046db074f3fa8806336076c769ca18d45af378af
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 16:49:10 +0700

    Add files via upload

---
+++ Return Value 2/Return Value 2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MyApplication
+{
+    class Program
+    {
+        static int MyMethod(int x, int y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyMethod(5, 3));
+        }
+    }
+}
+/*
+using System;
+namespace Application
+{
+    class Program
+    {
+      static int MyMethod(int x, int y)
+      {
+           return x + y;
+      }
+      static void Main(string[] args)
+      {
+         Console.WriteLine(MyMethid(5, 3));
+      }
+    }
+}
+*/
---
+++ Return Value/Return Value/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MyApplication
+{
+    class Program
+    {
+        static int MyMethod(int x)
+        {
+            return 5 + x;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyMethod(3));
+        }
+    }
+}
