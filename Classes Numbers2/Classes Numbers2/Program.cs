ec357a208465d0560d225679954d4ea0fe0552ae
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 15:36:06 +0700

    Add files via upload

---
+++ Classes Numbers2/Classes Numbers2/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+using System;
+namespace MyApplication
+{
+    class Car
+    {
+        string model;
+        string color;
+        int year;
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "red";
+            Ford.year = 2024;
+
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.color = "blue";
+            Opel.year = 2005;
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
+
+        }
+    }
+}
+*/
+using System;
+namespace MyApplication
+{
+    class Car
+    {
+        string color; // field
+        int maxSpeed; // field
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can");
+        }
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.fullThrottle();
+        }
+    }
+}
+
