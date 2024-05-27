2fee88218384afe7ca8e3ca346be2c0c0f91b9b3
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 15:41:09 +0700

    Add files via upload

---
+++ Continue/Continue/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+                
+        }
+    }
+}
