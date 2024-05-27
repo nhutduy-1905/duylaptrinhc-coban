23e54467480c9090cc56272141430434c22de981
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 15:58:24 +0700

    Add files via upload

---
+++ For Loop/For Loop/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+           for (int i = 1; i <=2; i++)
+            {
+                Console.WriteLine("Outer:" + i);                          
+                for(int j = 1; j <= 3; j++)
+                {
+                   Console.WriteLine("Inner:" + j);
+                }               
+            }
+        }
+    }
+
+}
