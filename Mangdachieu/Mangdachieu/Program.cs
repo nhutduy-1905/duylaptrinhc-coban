b8882c9b574c0b2d852dc813bb95e1f4105e7f20
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 16:28:57 +0700

    Add files via upload

---
+++ Mangdachieu/Mangdachieu/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[,] numbers = { { 1, 4, 2 }, { 3, 5, 6 } };
+//Truy ca[ phan tu mang 2D
+Console.WriteLine(numbers[0, 2]);
+numbers[0, 0] = 5;
+Console.WriteLine(numbers[0, 0]);
+
