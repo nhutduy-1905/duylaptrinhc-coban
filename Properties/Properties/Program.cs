3a820caa2c422bcae22e03e6b7d8e4641d7a3109
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 16:47:17 +0700

    Add files via upload

---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MyApplication
+{
+    class Person
+    {
+        private string name; // field
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }  // property 
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
