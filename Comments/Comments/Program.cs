59b9c512df42d70992c0bf26c891f26e481d6149
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 15:38:19 +0700

    Add files via upload

---
+++ Comments/Comments/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Bình luận // c# sẽ bỏ qua
+
+// This is a Comments
+Console.WriteLine("Hello Phan Hoang Nguyen"); // có thể comments ở đây
+
+// Comment nhiều dòng bắt đầu bằng 
+/*
+ * ở giữa ghi gì sẽ bị bỏ qua
+*/
+
+
