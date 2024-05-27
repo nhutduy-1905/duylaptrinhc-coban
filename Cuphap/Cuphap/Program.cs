91d31d8143f4cea1ac048c6a7d86aa82226f3dc3
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 15:44:22 +0700

    Add files via upload

---
+++ Cuphap/Cuphap/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+        }
+    }
+}
+// 1.using System sử dụng các lớp  từ System không gian tên
+// 2.c# bỏ qua khoảng trắng 
+// 3.namespace sử dụng để tổ chức mã và là nơi chứa các lớp và các không gian tên khác
+// 4.Dấu {} đánh dấu bắt đầu và kết thúc
+// 5. class chứa dữ liệu và phương thức. 
+// 7. Main là một phương thức.Bất kì đoạn mã naò trong ngoặc nhọn sẽ được thực thi
+// 9. Console là một lớp không gian  tên System , Có một WriteLine() phương được sử dụng để in ra
+// Nếu bỏ qua using system bạn sẽ phải viết System.Console.WriteLine() để in ra
+// c# kết thúc bằng dấu ;
