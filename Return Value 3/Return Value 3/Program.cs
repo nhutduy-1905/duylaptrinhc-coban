5dee507b983942dc373beb8756ccf8a9bb08fb93
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 16:55:15 +0700

    Add files via upload

---
+++ Return Value 3/Return Value 3/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MyApplication
+{
+    class Program
+    {
+        static int MyMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            int z = MyMethod(5, 3);
+            Console.WriteLine(z);
+        }
+    }
+}
