680c27118bcb6f54853c68a6c09079cd155ce5e0
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 16:27:19 +0700

    Add files via upload

---
+++ lapquamanghaichieu/lapquamanghaichieu/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Runtime.CompilerServices;
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
