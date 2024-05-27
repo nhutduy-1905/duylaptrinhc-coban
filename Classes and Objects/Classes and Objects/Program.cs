6caa3c75f27feac09327ec3c47635fa6c196afed
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 15:09:28 +0700

    Add files via upload

---
+++ Classes and Objects/Classes and Objects/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MyApplication
+{
+    class Car 
+    {
+        string color = "red";
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            // Tao nhieu doi tuong 
+            Car myObj2 = new Car();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj2.color);
+        }
+    }
+
+}
+/*
+class Car
+{
+    public string color = "red";
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car myObj3 = new Car();
+        Console.WriteLine(myObj3.color);
+    }
+}
+*/
