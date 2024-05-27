11fd9e27b1b75f2a0c501026bfe5560dcbb7ad3d
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 15:40:22 +0700

    Add files via upload

---
+++ Constants/Constants/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Thêm từ const
+ */
+const int myNum = 10;
+//myNum = 20; Hằng số không thể thay đôi được 
+Console.WriteLine(myNum);
