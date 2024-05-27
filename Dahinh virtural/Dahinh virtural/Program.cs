cc83db34988e11bd1f8ab82390b54ba651ce7d91
nhutduy-1905 <baominh10cn@gmail.com>
2024-05-27 15:48:36 +0700

    Add files via upload

---
+++ Dahinh virtural/Dahinh virtural/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MyApplication
+{
+    class Animal
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The dog says: bow bow");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal myPig = new Pig();
+            Animal myDog = new Dog();
+            
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.animalSound();
+        }
+    }
+}
